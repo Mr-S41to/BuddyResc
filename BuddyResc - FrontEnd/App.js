f0583f8bd41744d824f7a381dd7a3cc5341ebeaa
WarWolfOne <matheussaito95k@gmail.com>
2022-06-18 15:21:49 -0300

    Perecisa colocar o Gesture Handler, algo assim. Não consegui rodar a maquina virtual.

---
+++ BuddyResc - FrontEnd/App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Home from "./paginas/Home";
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
